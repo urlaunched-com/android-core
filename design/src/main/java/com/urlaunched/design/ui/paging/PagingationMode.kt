e6caecc3358e2d5a2a7c8f7c79715eeb86316b0c
Nazar Rusnak <inazarrusnak@gmail.com>
2024-04-02 16:12:00 +0300

    Import paging containers, make them design system agnostic

---
+++ design/src/main/java/com/urlaunched/design/ui/paging/PagingationMode.kt
@@ -0,0 +1,13 @@
+package com.urlaunched.design.ui.paging
+
+import androidx.compose.runtime.ProvidableCompositionLocal
+import androidx.compose.runtime.staticCompositionLocalOf
+
+enum class LocalPagingModeEnum {
+    READY,
+    LOADING,
+    ERROR,
+    EMPTY
+}
+
+val LocalPagingMode: ProvidableCompositionLocal<LocalPagingModeEnum?> = staticCompositionLocalOf { null }
