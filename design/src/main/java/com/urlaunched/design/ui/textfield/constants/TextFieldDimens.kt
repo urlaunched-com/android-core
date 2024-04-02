d56c332e598df27e0dafff174f897bd9d49c4cf9
Nazar Rusnak <inazarrusnak@gmail.com>
2024-04-02 16:58:05 +0300

    Add textfield components

---
+++ design/src/main/java/com/urlaunched/design/ui/textfield/constants/TextFieldDimens.kt
@@ -0,0 +1,8 @@
+package com.urlaunched.design.ui.textfield.constants
+
+import androidx.compose.ui.unit.dp
+
+internal object TextFieldDimens {
+    val cornersRadius = 10.dp
+    val borderSize = 1.dp
+}
