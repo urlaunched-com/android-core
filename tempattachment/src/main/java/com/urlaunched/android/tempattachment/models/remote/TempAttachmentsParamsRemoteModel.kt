598ab0d2f19ff42115df7e6e08f262af1389d864
Nazar Rusnak <inazarrusnak@gmail.com>
2024-04-05 10:32:01 +0300

    Temp attachment utilities

---
+++ tempattachment/src/main/java/com/urlaunched/android/tempattachment/models/remote/TempAttachmentsParamsRemoteModel.kt
@@ -0,0 +1,10 @@
+package com.urlaunched.android.tempattachment.models.remote
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class TempAttachmentsParamsRemoteModel(
+    @SerialName("filename")
+    val fileName: String
+)
