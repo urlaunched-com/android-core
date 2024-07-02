e9ebf2471c42512ec1a2e2e4ebdbeb2eaa34861b
VolodymyrLesko <lescko00@gmail.com>
2024-07-02 11:13:22 +0300

    Reverted test environment override and Paparazzi version

--- snapshottesting/src/main/java/com/urlaunched/android/snapshottesting/BaseSnapshotTest.kt
+++ snapshottesting/src/main/java/com/urlaunched/android/snapshottesting/BaseSnapshotTest.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalInspectionMode
 import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.Paparazzi
+import app.cash.paparazzi.detectEnvironment
 import com.android.ide.common.rendering.api.SessionParams
 import com.android.resources.NightMode
 import com.urlaunched.android.design.ui.paging.LocalPagingMode
@@ -26,7 +27,10 @@ abstract class BaseSnapshotTest(
             deviceConfig = DeviceConfig.PIXEL_5,
             renderingMode = renderingMode,
             showSystemUi = false,
-            maxPercentDifference = 0.1
+            maxPercentDifference = 0.1,
+            environment = detectEnvironment().run {
+                copy(compileSdkVersion = 33, platformDir = platformDir.replace("34", "33"))
+            }
         )
 
     fun snapshot(pagingMode: LocalPagingModeEnum? = null, composable: @Composable () -> Unit) {
