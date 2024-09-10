1743d1bc9e91364ac979a9b777371fb1874be745
Nazar Rusnak <inazarrusnak@gmail.com>
2024-09-10 10:24:05 +0300

    Implement additional response utils

--- common/src/main/java/com/urlaunched/android/common/response/Response.kt
+++ common/src/main/java/com/urlaunched/android/common/response/Response.kt
@@ -1,5 +1,7 @@
 package com.urlaunched.android.common.response
 
+import kotlinx.coroutines.CancellationException
+
 sealed class Response<T : Any> {
     data class Success<T : Any>(var data: T) : Response<T>()
     data class Error<T : Any>(val error: ErrorData) : Response<T>()
@@ -7,7 +9,7 @@ sealed class Response<T : Any> {
 
 data class ErrorData(val code: Int?, val message: String?, val errorKeys: List<String> = emptyList())
 
-fun <T : Any, R : Any> Response<T>.map(convert: (T) -> R): Response<R> = try {
+inline fun <T : Any, R : Any> Response<T>.map(convert: (T) -> R): Response<R> = try {
     when (this) {
         is Response.Error -> Response.Error(error)
         is Response.Success -> Response.Success(convert(data))
@@ -19,6 +21,54 @@ fun <T : Any, R : Any> Response<T>.map(convert: (T) -> R): Response<R> = try {
             message = e.message
         )
     )
+}
+
+inline fun <T, R : Any> T.wrapResponse(block: T.() -> R): Response<R> = runCatching(block).fold(
+    onSuccess = { Response.Success(it) },
+    onFailure = {
+        if (it is CancellationException) {
+            throw it
+        } else {
+            Response.Error(
+                ErrorData(
+                    code = null,
+                    message = it.message
+                )
+            )
+        }
+    }
+)
+
+inline fun <T, R : Any> T.wrapResponseFlatten(block: T.() -> Response<R>): Response<R> = runCatching(block).fold(
+    onSuccess = { it },
+    onFailure = {
+        if (it is CancellationException) {
+            throw it
+        } else {
+            Response.Error(
+                ErrorData(
+                    code = null,
+                    message = it.message
+                )
+            )
+        }
+    }
+)
+
+inline fun <T : Any> Response<T>.onSuccess(block: (T) -> Unit): Response<T> {
+    if (this is Response.Success) {
+        block(data)
+    }
+
+    return this
+}
+
+fun <T : Any, R : Any> Response.Error<T>.mapError(): Response<R> = Response.Error(error)
+
+fun <T : Any> Response<T>.getOrNull(): T? = if (this is Response.Success) {
+    this.data
+} else {
+    null
 }
 
 object ErrorCodes {
