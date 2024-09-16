b7aef7051c9d18e84c87e40e25bef2970eb12ded
Stanislav Ovchynnikov <stanislav.ovchinnikov.90@gmail.com>
2024-09-16 16:14:33 +0300

    add firebase performance

---
+++ common/src/main/java/com/urlaunched/android/common/firebaseperformance/LogScreenPerformanceAndTimeOnScreen.kt
@@ -0,0 +1,36 @@
+package com.urlaunched.android.common.firebaseperformance
+
+import android.app.Activity
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.platform.LocalContext
+import com.google.firebase.perf.FirebasePerformance
+import com.google.firebase.perf.application.FrameMetricsRecorder
+
+@Composable
+fun LogScreenPerformanceAndTimeOnScreen(route: String) {
+    val routeName = route.drop(1)
+    val activity = LocalContext.current as Activity
+    val trace = FirebasePerformance.getInstance().newTrace(routeName).apply { start() }
+
+    DisposableEffect(route) {
+        val recorder = FrameMetricsRecorder(activity).apply { start() }
+
+        onDispose {
+            val metrics = recorder.stop()
+
+            if (metrics.isAvailable) {
+                val frameMetrics = metrics.get()
+                trace.putMetric(FROZEN_FRAMES_TAG, frameMetrics.frozenFrames.toLong())
+                trace.putMetric(SLOW_FRAMES_TAG, frameMetrics.slowFrames.toLong())
+                trace.putMetric(TOTAL_FRAMES_TAG, frameMetrics.totalFrames.toLong())
+            }
+
+            trace.stop()
+        }
+    }
+}
+
+const val FROZEN_FRAMES_TAG = "frozen_frames"
+const val SLOW_FRAMES_TAG = "slow_frames"
+const val TOTAL_FRAMES_TAG = "total_frames"
