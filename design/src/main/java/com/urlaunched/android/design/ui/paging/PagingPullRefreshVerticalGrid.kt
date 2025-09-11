e4fcdd25bf77d1ed6d5d81ef32d6fc870b914d03
Nazar Rusnak <inazarrusnak@gmail.com>
2025-09-11 15:13:20 +0300

    Don't show loading state when previously loaded data is present

--- design/src/main/java/com/urlaunched/android/design/ui/paging/PagingPullRefreshVerticalGrid.kt
+++ design/src/main/java/com/urlaunched/android/design/ui/paging/PagingPullRefreshVerticalGrid.kt
@@ -246,15 +246,15 @@ fun <T : Any> PagingPullRefreshVerticalGrid(
                 startItems?.invoke(this, pagingState)
 
                 when {
-                    pagingState.isLoading -> {
-                        items(placeholderItemsNum) { index ->
-                            placeholderItem(index)
-                        }
-                    }
-
                     pagingState.isNoItems -> {
                         item(span = { GridItemSpan(maxLineSpan) }) {
                             noItemsPlaceholder()
+                        }
+                    }
+
+                    pagingState.isLoading -> {
+                        items(placeholderItemsNum) { index ->
+                            placeholderItem(index)
                         }
                     }
 
