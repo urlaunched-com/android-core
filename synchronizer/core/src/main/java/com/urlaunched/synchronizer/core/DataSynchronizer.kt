2b0be9107c8a1b4a6e9284cd811c7bdb7e1e894f
Nazar Rusnak <inazarrusnak@gmail.com>
2025-09-22 17:10:44 +0300

    Fix related model paging update, move paging extensions outside of the object

--- synchronizer/core/src/main/java/com/urlaunched/synchronizer/core/DataSynchronizer.kt
+++ synchronizer/core/src/main/java/com/urlaunched/synchronizer/core/DataSynchronizer.kt
@@ -5,6 +5,9 @@ import androidx.paging.cachedIn
 import androidx.paging.filter
 import androidx.paging.map
 import com.urlaunched.android.synchonizer.model.Synchronizable
+import com.urlaunched.synchronizer.core.DataSynchronizer.cancelDeleteModel
+import com.urlaunched.synchronizer.core.DataSynchronizer.deletedModel
+import com.urlaunched.synchronizer.core.DataSynchronizer.updateModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -25,131 +28,6 @@ object DataSynchronizer {
     val updateModel: MutableSharedFlow<Synchronizable<*>> = MutableSharedFlow()
     val deletedModel: MutableSharedFlow<Synchronizable<*>> = MutableSharedFlow()
     val cancelDeleteModel: MutableSharedFlow<Synchronizable<*>> = MutableSharedFlow()
-
-    inline fun <reified ACTUAL : Synchronizable<ID>, reified RELATED : Synchronizable<ID>, ID> Flow<PagingData<ACTUAL>>.synchronizeRelatedModel(
-        viewModelScope: CoroutineScope,
-        crossinline mapRelatedToActual: (ACTUAL, RELATED) -> ACTUAL,
-        crossinline mapActualToRelated: (ACTUAL) -> RELATED,
-        coroutineDispatcher: CoroutineDispatcher = Dispatchers.IO
-    ): Flow<PagingData<ACTUAL>> {
-        val accumulatedUpdates = MutableStateFlow<Map<ID, ACTUAL>>(mapOf())
-        val updatedData = mutableSetOf<ID>()
-
-        viewModelScope.launch(coroutineDispatcher) {
-            updateModel
-                .filterIsInstance<Synchronizable<ID>>()
-                .filter { it is RELATED }
-                .collectLatest { modelUpdate ->
-                    accumulatedUpdates.update { currentData ->
-                        val currentItem = currentData[modelUpdate.id]
-
-                        val mappedItem = if (modelUpdate is RELATED) {
-                            currentItem?.let { current ->
-                                mapRelatedToActual(current, modelUpdate)
-                            }
-                        } else {
-                            modelUpdate as ACTUAL
-                        }
-
-                        if (mappedItem != null) {
-                            currentData + Pair(mappedItem.id, mappedItem)
-                        } else {
-                            currentData
-                        }
-                    }
-                }
-        }
-
-        return combine(
-            this
-                .onEach {
-                    updatedData.clear()
-                }
-                .map { pagingData ->
-                    pagingData.map { item ->
-                        if (!updatedData.contains(item.id)) {
-                            updateModel.emit(mapActualToRelated(item))
-                            updatedData.add(item.id)
-                        }
-
-                        item
-                    }
-                }
-                .cachedIn(viewModelScope),
-            accumulatedUpdates
-        ) { pagingData, updates ->
-            pagingData.map { item ->
-                updates[item.id] ?: item
-            }
-        }.cachedIn(viewModelScope)
-    }
-
-    inline fun <reified T : Synchronizable<ID>, ID> Flow<PagingData<T>>.synchronize(
-        viewModelScope: CoroutineScope,
-        coroutineDispatcher: CoroutineDispatcher = Dispatchers.IO
-    ): Flow<PagingData<T>> {
-        val accumulatedUpdates = MutableStateFlow<Map<ID, T>>(mapOf())
-        val accumulatedDeletions = MutableStateFlow<Map<ID, T>>(mapOf())
-        val updatedData = mutableSetOf<ID>()
-
-        viewModelScope.launch(coroutineDispatcher) {
-            updateModel
-                .filterIsInstance<T>()
-                .collectLatest { modelUpdate ->
-                    accumulatedUpdates.update { currentData ->
-                        currentData + Pair(modelUpdate.id, modelUpdate)
-                    }
-                }
-        }
-
-        viewModelScope.launch(coroutineDispatcher) {
-            deletedModel
-                .filterIsInstance<T>()
-                .collectLatest { modelDeletion ->
-                    accumulatedDeletions.update { currentData ->
-                        currentData + Pair(modelDeletion.id, modelDeletion)
-                    }
-                }
-        }
-
-        viewModelScope.launch(coroutineDispatcher) {
-            cancelDeleteModel
-                .filterIsInstance<T>()
-                .collectLatest { modelDeletionCancellation ->
-                    accumulatedDeletions.update { currentData ->
-                        currentData - modelDeletionCancellation.id
-                    }
-                }
-        }
-
-        return combine(
-            this
-                .onEach {
-                    updatedData.clear()
-                }
-                .map { pagingData ->
-                    pagingData.map { item ->
-                        if (!updatedData.contains(item.id)) {
-                            updateModel.emit(item)
-                            updatedData.add(item.id)
-                        }
-
-                        item
-                    }
-                }
-                .cachedIn(viewModelScope),
-            accumulatedUpdates,
-            accumulatedDeletions
-        ) { pagingData, updatedData, deletedData ->
-            pagingData
-                .filter { item ->
-                    !deletedData.contains(item.id)
-                }
-                .map { item ->
-                    updatedData[item.id] ?: item
-                }
-        }.cachedIn(viewModelScope)
-    }
 
     suspend inline fun <reified ACTUAL : Synchronizable<ID>, ID> synchronizeList(
         crossinline listGetter: () -> List<ACTUAL>,
@@ -256,4 +134,114 @@ object DataSynchronizer {
     suspend fun emitCancelDelete(value: Synchronizable<*>) {
         cancelDeleteModel.emit(value)
     }
+}
+
+inline fun <reified ACTUAL : Synchronizable<ID>, reified RELATED : Synchronizable<ID>, ID> Flow<PagingData<ACTUAL>>.synchronizeRelatedModel(
+    viewModelScope: CoroutineScope,
+    crossinline mapRelatedToActual: (ACTUAL, RELATED) -> ACTUAL,
+    crossinline mapActualToRelated: (ACTUAL) -> RELATED,
+    coroutineDispatcher: CoroutineDispatcher = Dispatchers.IO
+): Flow<PagingData<ACTUAL>> {
+    val accumulatedUpdates = MutableStateFlow<Map<ID, RELATED>>(mapOf())
+    val updatedData = mutableSetOf<ID>()
+
+    viewModelScope.launch(coroutineDispatcher) {
+        updateModel
+            .filterIsInstance<RELATED>()
+            .collectLatest { modelUpdate ->
+                accumulatedUpdates.update { currentData ->
+                    currentData + Pair(modelUpdate.id, modelUpdate)
+                }
+            }
+    }
+
+    return combine(
+        this
+            .onEach {
+                updatedData.clear()
+            }
+            .map { pagingData ->
+                pagingData.map { item ->
+                    if (!updatedData.contains(item.id)) {
+                        updateModel.emit(mapActualToRelated(item))
+                        updatedData.add(item.id)
+                    }
+
+                    item
+                }
+            }
+            .cachedIn(viewModelScope),
+        accumulatedUpdates
+    ) { pagingData, updates ->
+        pagingData.map { item ->
+            updates[item.id]?.let { mapRelatedToActual(item, it) } ?: item
+        }
+    }.cachedIn(viewModelScope)
+}
+
+inline fun <reified T : Synchronizable<ID>, ID> Flow<PagingData<T>>.synchronize(
+    viewModelScope: CoroutineScope,
+    coroutineDispatcher: CoroutineDispatcher = Dispatchers.IO
+): Flow<PagingData<T>> {
+    val accumulatedUpdates = MutableStateFlow<Map<ID, T>>(mapOf())
+    val accumulatedDeletions = MutableStateFlow<Map<ID, T>>(mapOf())
+    val updatedData = mutableSetOf<ID>()
+
+    viewModelScope.launch(coroutineDispatcher) {
+        updateModel
+            .filterIsInstance<T>()
+            .collectLatest { modelUpdate ->
+                accumulatedUpdates.update { currentData ->
+                    currentData + Pair(modelUpdate.id, modelUpdate)
+                }
+            }
+    }
+
+    viewModelScope.launch(coroutineDispatcher) {
+        deletedModel
+            .filterIsInstance<T>()
+            .collectLatest { modelDeletion ->
+                accumulatedDeletions.update { currentData ->
+                    currentData + Pair(modelDeletion.id, modelDeletion)
+                }
+            }
+    }
+
+    viewModelScope.launch(coroutineDispatcher) {
+        cancelDeleteModel
+            .filterIsInstance<T>()
+            .collectLatest { modelDeletionCancellation ->
+                accumulatedDeletions.update { currentData ->
+                    currentData - modelDeletionCancellation.id
+                }
+            }
+    }
+
+    return combine(
+        this
+            .onEach {
+                updatedData.clear()
+            }
+            .map { pagingData ->
+                pagingData.map { item ->
+                    if (!updatedData.contains(item.id)) {
+                        updateModel.emit(item)
+                        updatedData.add(item.id)
+                    }
+
+                    item
+                }
+            }
+            .cachedIn(viewModelScope),
+        accumulatedUpdates,
+        accumulatedDeletions
+    ) { pagingData, updatedData, deletedData ->
+        pagingData
+            .filter { item ->
+                !deletedData.contains(item.id)
+            }
+            .map { item ->
+                updatedData[item.id] ?: item
+            }
+    }.cachedIn(viewModelScope)
 }
