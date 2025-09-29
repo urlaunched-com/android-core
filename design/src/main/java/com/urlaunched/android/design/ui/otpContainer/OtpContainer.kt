45242cc161f29a3cb498de42d47ebd74f452bca9
Khotych Mykola <kola13r@gmail.com>
2025-09-29 11:23:24 +0300

    Combine hasError and errorText into a single parameter

--- design/src/main/java/com/urlaunched/android/design/ui/otpContainer/OtpContainer.kt
+++ design/src/main/java/com/urlaunched/android/design/ui/otpContainer/OtpContainer.kt
@@ -35,26 +35,13 @@ import com.urlaunched.android.design.ui.otpContainer.models.OtpCellStyle
 
 private const val DEFAULT_OTP_LENGTH = 6
 
-/**
- * A composable OTP container input field where each character is shown in an individual box.
- *
- * @param modifier Modifier applied to the root of the text field.
- * @param otpLength The total number of characters expected in the OTP code. Default is 6.
- * @param hasError If true, the OTP boxes will show an error state (e.g., red border color).
- * @param otpText The current OTP input as a plain [String]. The cursor will automatically be placed at the end.
- * @param onOtpTextChange Callback invoked when the OTP value changes.
- * @param cellsArrangement Defines how the OTP boxes are arranged horizontally.
- * @param cellsStyle Configuration of the visual properties for each OTP cell, such as size, shape, text style, and border widths.
- * @param cellsColors Configuration of the color states (focused, error, empty, filled) and backgrounds for OTP cells.
- */
 @Composable
 fun OtpTextField(
     modifier: Modifier,
-    otpLength: Int = DEFAULT_OTP_LENGTH,
-    hasError: Boolean,
     otpText: String,
-    errorText: String,
     onOtpTextChange: (text: String) -> Unit,
+    error: String? = null,
+    otpLength: Int = DEFAULT_OTP_LENGTH,
     cellsArrangement: Arrangement.Horizontal = Arrangement.spacedBy(Dimens.spacingSmall),
     cellsStyle: OtpCellStyle = OtpCellStyle(),
     cellsColors: OtpCellColors = OtpCellColors()
@@ -79,7 +66,7 @@ fun OtpTextField(
                         OtpCellView(
                             index = index,
                             text = otpText,
-                            isError = hasError,
+                            isError = error != null,
                             isFocused = otpText.length == index,
                             style = cellsStyle,
                             colors = cellsColors
@@ -90,11 +77,11 @@ fun OtpTextField(
 
         )
 
-        if (hasError) {
+        if (!error.isNullOrEmpty()) {
             Spacer(Modifier.height(cellsStyle.errorTextTopPadding))
 
             Text(
-                text = errorText,
+                text = error,
                 style = cellsStyle.errorTextStyle,
                 color = cellsColors.errorBorderColor
             )
@@ -154,6 +141,45 @@ private fun OtpTextFieldPreview() {
     var textFieldValue by remember { mutableStateOf("12") }
 
     Box(
+        modifier = Modifier.background(Color.Black)
+    ) {
+        OtpTextField(
+            modifier = Modifier
+                .padding(vertical = 12.dp, horizontal = 16.dp)
+                .fillMaxWidth(),
+            otpText = textFieldValue,
+            onOtpTextChange = { textFieldValue = it },
+            cellsArrangement = Arrangement.SpaceBetween
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun OtpTextFieldErrorPreview() {
+    var textFieldValue by remember { mutableStateOf("123") }
+
+    Box(
+        modifier = Modifier.background(Color.Black)
+    ) {
+        OtpTextField(
+            modifier = Modifier
+                .padding(vertical = 12.dp, horizontal = 16.dp)
+                .fillMaxWidth(),
+            otpText = textFieldValue,
+            onOtpTextChange = { textFieldValue = it },
+            error = "Otp is incorrect, try another one",
+            cellsArrangement = Arrangement.SpaceBetween
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun OtpTextFieldErrorWithoutMessagePreview() {
+    var textFieldValue by remember { mutableStateOf("123456") }
+
+    Box(
         modifier = Modifier.background(Color.Black),
         contentAlignment = Alignment.TopCenter
     ) {
@@ -163,9 +189,8 @@ private fun OtpTextFieldPreview() {
                 .fillMaxWidth(),
             otpText = textFieldValue,
             onOtpTextChange = { textFieldValue = it },
-            errorText = "Otp is incorrect, try another one",
-            cellsArrangement = Arrangement.SpaceBetween,
-            hasError = false
+            error = "",
+            cellsArrangement = Arrangement.SpaceBetween
         )
     }
 }
